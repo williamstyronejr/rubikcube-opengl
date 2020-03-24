2311f9b76c6f98dd7144dfae2d97e8444353c8af
williamstyronejr <itenory@gmail.com>
2020-03-24 02:08:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Rubik Cube 
+A cross-platform app of a playable rubik cube using C++ and OpenGL.
+
+## Build Steps
+
+### Install dependencies
+
+#### Windows
+##### CMake
+Download and install [CMake](http://www.cmake.org/cmake/resources/software.html).
+
+
+#### Linux 
+##### CMake
+    sudo apt-get install cmake 
+    
+#### MacOS
+##### CMake
+Download and install [CMake](http://www.cmake.org/cmake/resources/software.html).
+
+### Compile and Run
+    git clone https://github.com/williamstyronejr/rubikcube-opengl.git --recursive
+    cd rubikcube-opengl
+    cmake .
+    ./main
---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <GLFW/glfw3.h>
+
+void oldRendering() {
+    glBegin(GL_TRIANGLES); // Start drawing a triangle primitive
+    glVertex3f(-1.0f, 0.0f, 0.0f); // The bottom left point
+    glVertex3f(1.0f, 0.0f, 0.0f); // The bottom right point
+    glVertex3f(0.0f, 1.0f, 0.0f); // The top point
+    glEnd();
+}
+
+int main(void)
+{
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+        oldRendering();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
